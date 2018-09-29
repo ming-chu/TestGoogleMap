5df6615cbc32af2b967800bda73931462217125a
Kenneth Chu <kennth.chu@gmail.com>
2018-09-29 10:50:53 +0800

    Initial Commit

---
+++ TestGoogleMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGoogleMap
+//
+//  Created by Kenneth Chu on 29/9/2018.
+//  Copyright © 2018 vegantell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
